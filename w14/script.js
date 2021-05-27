05cc5565f1c112b2fb94560b738697181f3f845d
yourname <409416541l@gms.tku.edu.tw>
2021-05-27 13:21:03 +0800

    adding w14 files

--- w14/script.js
+++ w14/script.js
@@ -1,7 +1,13 @@
 const p = document.querySelector('.menu-btn');
 const mainMenu = document.querySelector('.main-nav .main-menu');
-
-// console.log('mainMenu' mainMenu)
+ 
+// console.log('mainMenu', mainMenu);
 p.addEventListener('click', function () {
-    mainMenu.classList.toggle('show')
-})
+  mainMenu.classList.toggle('show');
+  if (mainMenu.classList.contains('show')) {
+    p.innerHTML = `<i class="fas fa-times fa-2x"></i>`;
+  } 
+  else {
+    p.innerHTML = `<i class="fas fa-bars fa-2x"></i>`;
+  }
+});
